7ccd61166d6094a2446781cc0ae93877a2d21aa8
kletera <olivierdallecarbonare@hotmail.com>
2024-04-30 16:56:26 +0200

    Modification du fichier view_connexion.php

---
+++ view/view_connexion.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="./style/style.css">
+    <script src="./script/script.js"></script>
+    <title>Connexion</title>
+</head>
+<body>
+    <h1>Connexion</h1>
+    <form action="" method="post">
+        <p>Veuillez saisir votre identifiant :</p> 
+        <input type="text" name="login"> 
+        <p>Veuillez saisir votre mot de passe :</p> 
+        <input type="text" name="mdp"> 
+        <input type="submit" value="connexion"> 
+    </form>
+</body>
+</html>
